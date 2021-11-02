14ff2e34df99d7f2e39db49644f8b2fba9328dc1
Arthur Schamroth <schamrotharthur@gmail.com>
2021-11-02 12:49:53 +0100

    Ajout des collectes avec favoris fonctionnels sur DB

--- app/src/main/java/com/example/integration/adapter/CollecteAdapter.kt
+++ app/src/main/java/com/example/integration/adapter/CollecteAdapter.kt
@@ -9,6 +9,7 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.integration.CollecteModel
+import com.example.integration.CollecteRepository
 import com.example.integration.EventActivity
 import com.example.integration.R
 
@@ -22,6 +23,7 @@ class CollecteAdapter(private val context: EventActivity,
         val collecteImage = view.findViewById<ImageView>(R.id.image_item)
         val collecteName:TextView? = view.findViewById(R.id.name_item)
         val collecteLocalisation:TextView? = view.findViewById(R.id.description_item)
+        val starIcon = view.findViewById<ImageView>(R.id.star_icon)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -34,14 +36,34 @@ class CollecteAdapter(private val context: EventActivity,
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         // récupérer informations de la collecte
         val currentCollecte = collecteList[position]
+
+        // récupérer repository
+        val repo = CollecteRepository()
+
         // utiliser glide pour utiliser l'url de l'image pour récupérer l'image depuis internet
         Glide.with(context).load(Uri.parse(currentCollecte.imageUrl)).into(holder.collecteImage)
 
         // mettre à jour le nom de la collecte
         holder.collecteName?.text = currentCollecte.nom
 
-        // mettre à jjour la description de la collecte
+        // mettre à jour la description de la collecte
         holder.collecteLocalisation?.text = currentCollecte.localisation
+
+        // vérifier si la collecte est likée ou pas
+        if(currentCollecte.liked){
+            holder.starIcon.setImageResource(R.drawable.ic_start)
+        }
+        else{
+            holder.starIcon.setImageResource(R.drawable.ic_unstar)
+        }
+
+        // rajouter interaction avec étoile
+        holder.starIcon.setOnClickListener{
+            //inverse si le bouton est liké ou non
+            currentCollecte.liked = !currentCollecte.liked
+            // mettre à jour la collecte
+            repo.updateCollecte(currentCollecte)
+        }
     }
 
     override fun getItemCount(): Int = collecteList.size
