efc1d951db5328d62c1cf90d555072adf3272b02
Cédric De Dryver <c.dedryver@students.ephec.be>
2021-12-16 03:55:17 +0100

    Ajout de nouvelles variables a la DB dans LoginActivity, ProfileActivity affiche le nom et les points d'utilisateur, moderation activé pas encore fonctionnelle

--- app/src/main/java/com/example/integration/ProfileActivity.kt
+++ app/src/main/java/com/example/integration/ProfileActivity.kt
@@ -5,27 +5,27 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.Switch
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.SwitchCompat
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import com.google.firebase.firestore.SetOptions
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class ProfileActivity : AppCompatActivity() {
+
+    val db = Firebase.firestore
     private var mail = ""
-    private var moderator = true
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
-        if(moderator===true) {
-            val moderator_sidebar = findViewById<View>(R.id.moderator_sidebar)
-            val moderator_activity = findViewById<Button>(R.id.moderator_btn_activity)
-            moderator_sidebar.visibility = View.VISIBLE
-            moderator_activity.visibility = View.VISIBLE
+        mail = intent.getStringExtra("key").toString()
 
-        }
-        mail=intent.getStringExtra("key").toString()
-
-        val btn_parametre = findViewById<Button>(R.id.profile_btn_paramètre)
+        val btn_parametre = findViewById<Button>(R.id.profile_btn_parametre)
         val btn_historique = findViewById<Button>(R.id.profile_btn_historique)
 
 
@@ -38,7 +38,30 @@ class ProfileActivity : AppCompatActivity() {
             val intent = Intent(this, ParametreActivity::class.java)
             startActivity(intent)
         }
+        setupCompontents()
+    }
 
+    private fun setupCompontents() {
+        db.collection("clients")
+            .get()
+            .addOnSuccessListener { result ->
+                for(document in result){
+                    if(document.data.getValue("email").toString() == mail ) {
+                        val points = document.data.getValue("points").toString()
+                        val name = document.data.getValue("name").toString()
+                        findViewById<TextView>(R.id.profile_points).text = points
+                        findViewById<TextView>(R.id.profile_name).text = name
+                        if (document.data.getValue("moderator") == true) {
+                            val moderator_sidebar =
+                                findViewById<View>(R.id.moderator_sidebar)
+                            val moderator_activity =
+                                findViewById<Button>(R.id.moderator_btn_activity)
+                            moderator_sidebar.visibility = View.VISIBLE
+                            moderator_activity.visibility = View.VISIBLE
+                        }
+                    }
+                }
+            }
     }
 
 }
