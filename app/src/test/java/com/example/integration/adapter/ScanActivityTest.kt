47f5a704bc68f6574cd8ddab3cbec89c6a5b04cf
Andreas Bombaert <a.bombaert@students.ephec.be>
2021-12-22 18:11:44 +0100

    quelques tests sur us8 et us12

---
+++ app/src/test/java/com/example/integration/adapter/ScanActivityTest.kt
@@ -0,0 +1,29 @@
+package com.example.integration.adapter
+
+import android.content.Intent
+import com.example.integration.ScanActivityUtil
+import com.google.common.truth.Truth.assertThat
+import org.junit.Test
+import java.text.SimpleDateFormat
+import java.util.*
+
+
+class ScanActivityTest {
+
+    @Test
+    fun `"le retour est bloqué" return true`(){
+        val intent = Intent()
+        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        val result = ScanActivityUtil.avoidFreePoints(intent)
+        assertThat(result).isTrue()
+    }
+
+    @Test
+    fun `"le format de date est bon" return true`() {
+        val dateFormat = SimpleDateFormat("dd/M/yyyy");
+        val date = dateFormat.format(Date());
+        val result = ScanActivityUtil.verifyDateFormat(date)
+        assertThat(result).isTrue()
+    }
+
+}
